d541a1f00ca97ea6c9c6b25b597e75df267d95ce
UrielNunez <Devuriel1@gmail.com>
2021-12-02 00:22:03 -0600

    Cuarto Commit: Implementacion de SignIn

--- app/src/main/java/com/example/alumnos/Login/SignInActivity.kt
+++ app/src/main/java/com/example/alumnos/Login/SignInActivity.kt
@@ -1,12 +1,70 @@
 package com.example.alumnos.Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.alumnos.MainActivity
 import com.example.alumnos.R
+import com.example.alumnos.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding.signInAppCompatButton.setOnClickListener{
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.passwordEditText.text.toString()
+
+            when {
+                mEmail.isEmpty() || mPassword.isEmpty() -> {
+                    Toast.makeText(baseContext, "Correo o Contraseña Incorrectos.",
+                        Toast.LENGTH_SHORT).show()
+                } else -> {
+                    SignIn(mEmail, mPassword)
+                }
+            }
+
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun SignIn (email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("TAG", "signInWithEmail:success")
+                } else {
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Correo o Contraseña Incorrectos.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun reload(){
+        val intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
     }
 }
--- app/src/main/java/com/example/alumnos/MainActivity.kt
+++ app/src/main/java/com/example/alumnos/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.alumnos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.alumnos.databinding.ActivityMainBinding
+import com.example.alumnos.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
     }
 }
